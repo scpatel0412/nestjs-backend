7fafa9318445bab5bfb13380926df36c16e10de7
harsh.davda <harsh.davda@iflair.com>
2022-06-27 13:38:18 +0530

    added random image show section from unsplash

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
-import axios from 'axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
